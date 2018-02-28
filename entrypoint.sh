23b3385db4816f5eeeabc38cccec1fb8fac4a57a
Grégoire Weber <gweber@clever-age.com>
2018-02-27 23:01:21 -0500

    Add JVM option to avoid browser opening on youtrack startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx1g -XX:MaxMetaspaceSize=250M -Djava.awt.headless=true -jar youtrack-${YOUTRACK_VERSION}.${YOUTRACK_BUILD}.jar ${YOUTRACK_PORT}
+java -Xmx1g -XX:MaxMetaspaceSize=250M -Djava.awt.headless=true -Djetbrains.youtrack.disableBrowser=true -jar youtrack-${YOUTRACK_VERSION}.${YOUTRACK_BUILD}.jar ${YOUTRACK_PORT}
